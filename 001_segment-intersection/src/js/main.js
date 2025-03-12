74c4eecba9f54c7de4a103bcba4724424bcd1f19
Byron2016 <bgva2005@yahoo.com>
2025-03-12 18:26:15 -0500

    feat[001-segmentIntersection]: add segments and extreme points names

--- 001_segment-intersection/src/js/main.js
+++ 001_segment-intersection/src/js/main.js
@@ -1,3 +1,34 @@
 const myCanvas = document.getElementById("myCanvas");
 myCanvas.width = window.innerWidth;
 myCanvas.height = window.innerHeight;
+
+const A = { x: 200, y: 150 };
+const B = { x: 150, y: 250 };
+const C = { x: 50, y: 100 };
+const D = { x: 250, y: 200 };
+
+const ctx = myCanvas.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(A.x, A.y);
+ctx.lineTo(B.x, B.y);
+ctx.moveTo(C.x, C.y);
+ctx.lineTo(D.x, D.y);
+ctx.stroke();
+
+drawDot(A, "A");
+drawDot(B, "B");
+drawDot(C, "C");
+drawDot(D, "D");
+
+function drawDot(point, label) {
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(point.x, point.y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.font = "bold 14px Arial";
+  ctx.fillText(label, point.x, point.y);
+}
